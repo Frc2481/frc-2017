90be89d4573215499177fdbda3c8f17c5a01b29b
Kython89 <kylelanman@gmail.com>
2017-02-06 18:19:05 -0600

    VisionUpdateReceiver.h

---
+++ r2017/src/Vision/VisionUpdateReceiver.h
@@ -0,0 +1,20 @@
+/*
+ * VisionUpdateReceiver.h
+ *
+ *  Created on: Feb 3, 2017
+ *      Author: kyle
+ */
+
+#ifndef SRC_VISION_VISIONUPDATERECEIVER_H_
+#define SRC_VISION_VISIONUPDATERECEIVER_H_
+
+#include "VisionUpdate.h"
+
+class VisionUpdateReceiver {
+public:
+	VisionUpdateReceiver();
+	virtual ~VisionUpdateReceiver();
+	virtual void gotUpdate(VisionUpdate update) = 0;
+};
+
+#endif /* SRC_VISION_VISIONUPDATERECEIVER_H_ */
